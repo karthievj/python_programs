5db6cd44e52d78b99ca161d0f362f256019e617c
karthievj <kakrish6@cisco.com>
2023-05-27 16:23:23 +0530

    commit

--- reverse_vowel_alone.py
+++ reverse_vowel_alone.py
@@ -1,3 +1,19 @@
+"""
+Test Cases for better understanding
+
+Example 1
+Input: s = "hello"
+Output: "holle"
+
+Example 2:
+Input: s = "leetcode"
+Output: "leotcede"
+
+Example 3:
+Input: s = "race car"
+Output: "race car"
+"""
+
 def reverseVowels(s):
     
     vowels = ["a","e","i","o","u","A","E","U","I","O"]
@@ -15,21 +31,17 @@ def reverseVowels(s):
 
       
     final = {}
-    print(vowel_track,"vowel_track")
-    print(result,"result")
     reverse = vowel_track[::-1]
-    print(reverse,"reverse")
     for idx,val in enumerate(reverse):
         final[idx]=val
-    print(final,"final")
     
     for i in result:
         if i in final:
             position_to_insert = result.index(i)
             result.remove(i)
             result.insert(position_to_insert, final[i])
-    print(result)
     
     return "".join(result)
+
 a = reverseVowels(s ="race car")
 print(a)
